1f89fe05d6f3cd4612a8cdd21e0b17909ccc92fe
Juan <jagames145@hotmail.com>
2022-05-31 14:20:34 -0500

    Backend 90%

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
+const { mongoose } = require('./database')
+
+
+//body capture
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// import routes
+const authRoutes = require('./routes/auth.js');
+
+app.use(express.json());
+
+// route middlewares
+app.use('/api/user',authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`server running on: ${PORT}`)
+})
+
